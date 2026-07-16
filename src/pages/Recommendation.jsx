1d788105e1f400f380be6e36df0cae70d4d56692
Tithi-Gajanand-Chauhan <tithigajanand27@gmail.com>
2026-07-16 09:08:23 +0530

    Complete frontend pages for group recommendation flow

--- src/pages/Recommendation.jsx
+++ src/pages/Recommendation.jsx
@@ -1,17 +1,39 @@
 function Recommendation() {
+  
+  const movie = {
+    title: "Interstellar",
+    genre: "Sci-Fi, Adventure",
+    language: "English",
+    duration: "2h 49m",
+    rating: "4.8",
+    poster: "https://via.placeholder.com/250x350",
+    reason: "Your group likes Sci-Fi, English movies, and longer watch times."
+  };
+  
   return (
     <div>
-      <h1>AI Recommendations</h1>
+      <h1>Your Recommendation</h1>
 
-      <div>
-        <h2>Interstellar</h2>
-        <p>
-          Recommended because the group prefers Sci-Fi,
-          emotional storytelling, and adventure.
-        </p>
+      <img src={movie.poster} alt={movie.title}/>
+      <h2>{movie.title}</h2>
 
-        <button>Vote</button>
-      </div>
+      <p><strong>Genre:</strong> {movie.genre}</p>
+
+      <p><strong>Language:</strong> {movie.language}</p>
+
+      <p><strong>Duration:</strong> {movie.duration}</p>
+
+      <p><strong>Rating:</strong> ⭐ {movie.rating}</p>
+
+      <h3>Why this recommendation?</h3>
+
+      <p>{movie.reason}</p>
+
+      <button>Watch Trailer</button>
+
+      <button>Recommend Another</button>
+
+
     </div>
   );
 }
